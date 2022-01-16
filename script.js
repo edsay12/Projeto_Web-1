0db01031157457913d40dd9391153078c1c8dd40
edsay12 <edvandearaujo2@hotmail.com>
2022-01-16 18:48:10 -0300

    header responsivo

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ window.addEventListener("scroll",()=>{
 
 
 // plans
+
+const buttonLinks = window.document.getElementById('openLinks')
+
+function nav_response(){
+    window.document.getElementById("logo").classList.toggle('logo_open')
+    window.document.getElementById('links').classList.toggle('link_open')
+    window.document.getElementById('header').classList.toggle('header_open')
+    window.document.querySelector('body').classList.toggle('display')
+
+    window.document.addEventListener('scroll',nav_responseWindow)
+
+}
+function nav_responseWindow(){
+    window.document.getElementById("logo").classList.remove('logo_open')
+    window.document.getElementById('links').classList.remove('link_open')
+    window.document.getElementById('header').classList.remove('header_open')
+    window.document.querySelector('body').classList.remove('display')
+}
+
+
+buttonLinks.addEventListener('click',nav_response)
